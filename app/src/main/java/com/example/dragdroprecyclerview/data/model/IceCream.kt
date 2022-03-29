bb897e07cae087f5aaf4e01f77c9a73df3630d20
lowjungxuan <abu@umchtech.com>
2022-03-29 19:18:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dragdroprecyclerview/data/model/IceCream.kt
@@ -0,0 +1,11 @@
+package com.example.dragdroprecyclerview.data.model
+
+class IceCream(
+        val name: String,
+        val price: Float,
+        val colorRed: Float,
+        val colorGreen: Float,
+        val colorBlue: Float) {
+
+    override fun toString() = name
+}
